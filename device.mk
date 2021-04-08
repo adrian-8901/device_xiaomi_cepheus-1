a0d4c05c9c159117c009e2f5cc9b5bf873e610fc
adrian-8901 <adrianlam230@gmail.com>
2021-04-08 20:23:18 +0800

    Device: Inherit FOD

--- device.mk
+++ device.mk
@@ -186,7 +186,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.cepheus
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.cepheus 
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
